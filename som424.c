cdf4b26579b47fa060037e6f8708fa2b1964247e
SOMYUUKTHAA <30828746+SOMYUUKTHAA@users.noreply.github.com>
2018-03-07 16:03:09 +0530

    Update som424.c

--- som424.c
+++ som424.c
@@ -2,21 +2,21 @@
 void main()
 { 
     char a[10000],d[10000];
-    int b=0,i,j,c=0;
+    int b=0,c=0,i;
     printf("\nenter two strings\n");
     scanf("%s%s",&a,&d);
     for(i=0;a[i]!='\0';i++)
     {
     b+=a[i];
     }
-    for(j=0;a[j]!='\0';j++)
+    for(i=0;d[i]!='\0';i++)
     {
-    c+=a[j];
+    c+=d[i];
     }
     if(b==c)
-    printf("%s",a);
+    printf("Strings are equal :%s",a);
     else if(b>c)
-    printf("%s",a);
+    printf("Bigger string : %s",a);
     else
-    printf("%s",c);
+    printf("Bigger string : %s",d);
 }
