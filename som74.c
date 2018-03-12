ecbcdecc2ba538a0ed3c2ec72964e775f95ef094
SOMYUUKTHAA <30828746+SOMYUUKTHAA@users.noreply.github.com>
2018-03-12 15:44:37 +0530

    Create som74.c

---
+++ som74.c
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+int main()
+{
+    float n;
+    int res;
+    cout<<"enter number:";
+    cin>>n;
+    res=round(n);
+    cout<<"Reault:"<<res;
+}
